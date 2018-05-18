aef37c81113e22d1100204e2270ecc51380aa9a2
pq5357 <pq5357@126.com>
2018-05-18 11:13:03 +0800

    更新了copy到固件的脚本

--- cpAppToSystem.bat
+++ cpAppToSystem.bat
@@ -1,2 +1,1 @@
-copy  D:\RacSystemTest\app\build\outputs\apk\debug\RacSystemTest.apk C:\Users\Admin\Desktop\RacSystemTest.apk
-copy  D:\RacSystemTest\app\build\outputs\apk\debug\RacSystemTest.apk \\172.16.14.80\shanggeng\rtbox\android-6.0.1-2.1.0\device\fsl\sabresd_6dq\files\RacSystemTest.apk
+copy  D:\RacSystemTest\app\build\outputs\apk\debug\RacSystemTest_MAIN.apk \\172.16.14.80\shanggeng\rtbox\android-6.0.1-2.1.0\device\fsl\sabresd_6dq\files\RacSystemTest.apk
